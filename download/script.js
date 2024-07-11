ba4979405391d0d6d75ca0eb0ff3f039c4a254a6
RandomBitByte <random98user@gmail.com>
2024-07-11 13:47:30 +0300

    Add files via upload

---
+++ download/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var message = document.getElementById("message");
+    var downloadLink = document.getElementById("download-link");
+
+    function isAndroid() {
+        return /Android/i.test(navigator.userAgent);
+    }
+
+    if (isAndroid()) {
+        message.textContent = "Your device is compatible!";
+        downloadLink.style.display = "inline-block";
+    } else {
+        message.textContent = "Sorry, this app is only available for Android devices.";
+    }
+});
